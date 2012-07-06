55dae164883ecf98fee2185c685699b9f2624e64
yonghuang <h00138136@163.com>
2012-07-06 16:03:58 +0800

    create database data generation script

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,33 @@
+import meishi.Shop
+
 class BootStrap {
 
     def init = { servletContext ->
+		return
+		
+		(1..20).each { i ->
+			def shop = new Shop(
+				name : "海底捞$i",
+				rating : 4.5,
+				phone : 12346798,
+				startPrice : 7,
+				description : "海底捞",
+				province : "江苏",
+				city : "南京",
+				district : "白下区",
+				road : "太平南路1号大行宫新世纪广场4楼(近中山东路)"
+				)
+			if (shop.validate()) {
+				shop.save(flush :true)
+		    }
+		    else {
+				shop.errors.allErrors.each {
+					println it
+				}
+		    }
+		}
     }
+	
     def destroy = {
     }
 }
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -10,6 +10,9 @@
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
+grails.gorm.default.constraints = {
+	'*'(nullable: true)
+}
 
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
