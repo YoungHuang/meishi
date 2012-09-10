94e605dfaa5c69c800636dd2384c2f432969851f
yonghuang <h00138136@163.com>
2012-09-10 13:59:11 +0800

    update

--- grails-app/domain/meishi/Order.groovy
+++ grails-app/domain/meishi/Order.groovy
@@ -7,6 +7,9 @@ import java.util.List;
 * 订单
 */
 class Order {
+	final static STATUS_NEW = 0;
+	final static STATUS_CONFIRM = 1;
+	
 	/** 商家 **/
 	Shop shop;
 	/** 总金额 **/
@@ -17,6 +20,8 @@ class Order {
 	Integer people = 0
 	/** 备注 **/
 	String description
+	/** 状态 **/
+	Integer status = STATUS_NEW
 	/** 订单列表 **/
 	List<OrderItem> orderItemList = new ArrayList<OrderItem>()
 	
